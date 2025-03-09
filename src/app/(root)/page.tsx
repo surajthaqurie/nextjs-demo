b6cc3d7569d206e5f6832ae8b805540cb327fa41
Suraj <surajchan68@gmail.com>
2025-03-09 11:09:31 +0545

    feat: add the header and footer component

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,7 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = { title: 'Home' };
+
+export default function Home() {
+  return <div>Home</div>;
+}
---
+++ src/lib/constants/index.ts
@@ -0,0 +1,2 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME;
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
